e4a0c039459d4c8c8b2eb70cb96e72be7c5aacf4
tjanicki <tjaygallery@gmail.com>
2019-03-03 12:29:33 +0100

    heroku-config

--- src/main/java/com/restaurant/management/config/DatabaseConfig.java
+++ src/main/java/com/restaurant/management/config/DatabaseConfig.java
@@ -13,46 +13,25 @@ import java.sql.SQLException;
 
 //@Configuration
 //public class DatabaseConfig {
-
-//    private static Logger LOGGER = LoggerFactory.getLogger(DatabaseConfig.class);
 //
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//    private String dbName = System.getProperty("RDS_DB_NAME");
+//    private String userName = System.getProperty("RDS_USERNAME");
+//    private String password = System.getProperty("RDS_PASSWORD");
+//    private String hostname = System.getProperty("RDS_HOSTNAME");
+//    private String port = System.getProperty("RDS_PORT");
 //
-//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-//        dataSource.setUsername("restaurant");
-//        dataSource.setPassword("restaurant");
-//        dataSource.setUrl(
-//                "jdbc:mysql://aa1uxsttl7z33a.cblujp7mksrf.eu-west-1.rds.amazonaws.com:3306/restaurant?useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false");
+//    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseConfig.class);
 //
-//        return dataSource;
-//    }
-
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//
-//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-//        dataSource.setUsername("restaurant");
-//        dataSource.setPassword("restaurant");
-//        dataSource.setUrl(
-//                "jdbc:mysql://restaurant.cblujp7mksrf.eu-west-1.rds.amazonaws.com:3306/restaurant?useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false");
-//
-//        return dataSource;
-//    }
-
 //    @Bean
 //    private static Connection getRemoteConnection() {
-//
-//        if (System.getProperty("aa1uxsttl7z33a.cblujp7mksrf.eu-west-1.rds.amazonaws.com") != null) {
+//        if (System.getenv("RDS_HOSTNAME") != null) {
 //            try {
-//                Class.forName("com.mysql.cj.jdbc.Driver");
-//                String dbName = System.getProperty("aa1uxsttl7z33a");
-//                String userName = System.getProperty("restaurant");
-//                String password = System.getProperty("restaurant");
-//                String hostname = System.getProperty("aa1uxsttl7z33a.cblujp7mksrf.eu-west-1.rds.amazonaws.com");
-//                String port = System.getProperty("RDS_PORT");
+//                Class.forName("org.mysql.Driver");
+//                String dbName = "ebdb";
+//                String userName = "restaurant";
+//                String password = "restaurant";
+//                String hostname = "aa1uxsttl7z33a.cblujp7mksrf.eu-west-1.rds.amazonaws.com";
+//                String port = "3306";
 //                String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
 //                LOGGER.trace("Getting remote connection with connection string from environment variables.");
 //                Connection con = DriverManager.getConnection(jdbcUrl);
@@ -64,5 +43,19 @@ import java.sql.SQLException;
 //        }
 //        return null;
 //    }
-//    jdbc:mysql://aa1uxsttl7z33a.cblujp7mksrf.eu-west-1.rds.amazonaws.com:3306/aa1uxsttl7z33a?useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false
+//
+////    @Bean
+////    public DataSource dataSource() {
+////        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+////
+////        String last = "?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&useSSL=false";
+////        String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + last;
+////
+////        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+////        dataSource.setUsername(userName);
+////        dataSource.setPassword(password);
+////        dataSource.setUrl(jdbcUrl);
+////        return dataSource;
+////    }
+//
 //}

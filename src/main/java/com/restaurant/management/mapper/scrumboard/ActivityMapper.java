5588e47b487476650a915cb9e497cd077313279a
Khartaz <tjaygallery@gmail.com>
2019-09-24 19:27:41 +0200

    scrumboard mapper model

---
+++ src/main/java/com/restaurant/management/mapper/scrumboard/ActivityMapper.java
@@ -0,0 +1,7 @@
+package com.restaurant.management.mapper.scrumboard;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public final class ActivityMapper {
+}
---
+++ src/main/java/com/restaurant/management/mapper/scrumboard/BoardMapper.java
@@ -0,0 +1,11 @@
+package com.restaurant.management.mapper.scrumboard;
+
+import com.restaurant.management.domain.scrumboard.Board;
+import com.restaurant.management.domain.scrumboard.dto.BoardDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class BoardMapper {
+
+
+}
